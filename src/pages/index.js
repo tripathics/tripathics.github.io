24b434feb443e0746b80db34c7fad2827c92038c
Chandrashekhar Tripathi <tripathics@outlook.com>
2023-07-13 00:11:02 +0530

    update links in projects

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,23 +11,19 @@ import { projectsList } from "../data/projectsList"
 const IndexPage = () => {
   const posts = [
     { name: 'Setting up Node on Linux', date: 'Jan 15, 2023', writeup: '/posts/setting-up-node-on-linux', tags: ["Setup", "Linux", "Web"] },
-    { name: 'My journey through Angular', date: 'Jun 5, 2022', writeup: '/posts/TODO', tags: ["Front-end", "TypeScript", "Walkthrough"] },
-    { name: 'Introduction to Linux Terminal', date: 'February 20, 2022', writeup: '/posts/TODO', tags: ["Command line", "Terminal", "Linux"] },
-    { name: 'Installing Arch Linux on a Virtualbox VM', date: 'January 27, 2022', writeup: '/posts/TODO', tags: ["Linux", "OS", "VM"] },
-    { name: 'Creating a simple typing game webapp', date: 'December 18, 2021', writeup: '/posts/TODO', tags: ["JavaScript", "Event", "API"] },
     { name: 'Setting up your first coding environment', date: 'August 17, 2020', writeup: '/posts/setting-up-your-first-coding-environment', tags: ["IDE", "C", "VsCode"] },
   ]
 
   return (
     <div className="home-component">
-      <Helmet title={config.siteTitle}/>
+      <Helmet title={config.siteTitle} />
 
       <header className="head-container hero">
         <h1 className="hero-heading" id="hello"><Link to="#hello">Hello!</Link></h1>
 
-        <p className="hero-description">I'm Chandrashekhar, a software developer and engineer who likes making projects from scratch and learning new technologies. I'm a computer science student pursuing B. Tech. from <Link to="https://www.nitap.ac.in/" target="_blank" rel="noreferrer">NIT Arunachal Pradesh</Link>. I like pop music, coding, sketching and shooting photos.
+        <p className="hero-description">I'm Chandrashekhar, a software developer and engineer who likes making projects from scratch and learning new technologies. I'm a computer science student pursuing B. Tech. from <Link to="https://www.nitap.ac.in/" target="_blank" rel="noreferrer">NIT Arunachal Pradesh</Link>. I like pop music, coding, sketching and shooting photos. Sometimes I write technical blogs.
           <br /><br />
-          This is the spot for finding my projects and articles I share with the world &#128518;</p>
+          This is the spot for finding my projects I share with the world &#128518;.</p>
       </header>
 
       <section className="container">
@@ -35,7 +31,7 @@ const IndexPage = () => {
       </section>
 
       <section className="container">
-        <Heading id='skills' title="Skills" />
+        <Heading id='skills' title="Technologies I work with" />
         <Skills />
       </section>
 
@@ -58,7 +54,7 @@ const IndexPage = () => {
       </section>
 
       <section className="container resume">
-        <Heading id='resume' title="Resume" extLinkText="View PDF" extLink='https://1drv.ms/b/s!AnLTSa_M6LzghoxPkZAZUAH33qjX7w?e=uVNHKh' />
+        <Heading id='resume' title="Resume" extLinkText="View PDF" extLink='https://raw.githubusercontent.com/tripathics/resume/7d73cc617db7e93212b576f35e8e79301ee46f45/Chandrashekhar.pdf' />
         <div className="image-wrapper">
           <img src={require("../assets/Chandrashekhar_Tripathi-resume.webp").default} alt="resume" width="925" height="1196" />
         </div>
--- src/pages/posts.js
+++ src/pages/posts.js
@@ -28,21 +28,6 @@ const Posts = () => {
       </section>
 
       <section className="container">
-        <h2 className="year">2022</h2>
-        <div className="posts">
-          <PostListItem title={"Python for C programmers"} date={"Jul 07"} />
-          <PostListItem title={"My Journey through Angular"} date={"Jun 05"} />
-          <PostListItem title={"Introduction to Linux Terminal"} date={"Feb 28"} />
-          <PostListItem title={"Installing Arch Linux on a Virtualbox VM"} date={"Jan 27"} />
-        </div>
-      </section>
-      <section className="container">
-        <h2 className="year">2021</h2>
-        <div className="posts">
-          <PostListItem title={"Creating a simple typing game webapp"} date={"Dec 18"} />
-        </div>
-      </section>
-      <section className="container">
         <h2 className="year">2020</h2>
         <div className="posts">
           <PostListItem title={"Setting up your first coding environment"} date={"Aug 17"} />
